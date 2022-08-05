8411279277dc85ca2cb1d17f63f7745316142b3f
Damian Oliva <damian.oliva@hotmail.com>
2022-08-05 12:08:56 -0300

    primera subida

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c6ce15bfa536bc8032979eda845439c6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6de6a329-1a4f-4fb0-bc2e-c2b8ba0ae125'
+};
